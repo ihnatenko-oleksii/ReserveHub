fb7757bfebc341a191e388c547df0e8a80f56079
ihnatenko-oleksii <ignatenkolesha123@gmail.com>
2025-05-04 20:02:16 +0200

    initial project

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '1rem', background: '#333', color: 'white' }}>
-      <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem' }}>
-        <li><Link to="/" style={{ color: 'white' }}>Home</Link></li>
-        <li><Link to="/services" style={{ color: 'white' }}>Services</Link></li>
-        <li><Link to="/reservations" style={{ color: 'white' }}>Reservations</Link></li>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/" style={{ color: 'white', textDecoration: 'none', fontSize: '1.5rem' }}>
+          ReserveHub
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/" className="navbar-link">Home</Link></li>
+        <li><Link to="/services" className="navbar-link">Services</Link></li>
+        <li><Link to="/reservations" className="navbar-link">Reservations</Link></li>
       </ul>
     </nav>
   );
