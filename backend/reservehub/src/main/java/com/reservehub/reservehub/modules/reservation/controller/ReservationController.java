ddb6b4ad940e6ff60537ad66f7898af4d14ac000
ihnatenko-oleksii <ignatenkolesha123@gmail.com>
2025-05-12 15:06:49 +0200

    CRUD for Reservation

--- backend/reservehub/src/main/java/com/reservehub/reservehub/modules/reservation/controller/ReservationController.java
+++ backend/reservehub/src/main/java/com/reservehub/reservehub/modules/reservation/controller/ReservationController.java
@@ -1,19 +1,44 @@
 package com.reservehub.reservehub.modules.reservation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reservehub.reservehub.modules.reservation.dto.ReservationDTO;
+import com.reservehub.reservehub.modules.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+    private final ReservationService reservationService;
 
-    @PostMapping("/")
-    String index(){
-        return "index";
+    @PostMapping
+    public ResponseEntity<ReservationDTO> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        return ResponseEntity.ok(reservationService.createReservation(reservationDTO));
     }
 
-    @GetMapping("/")
-    String index1(){
-        return "index";
+    @PutMapping("/{id}")
+    public ResponseEntity<ReservationDTO> updateReservation(
+            @PathVariable Long id,
+            @RequestBody ReservationDTO reservationDTO) {
+        return ResponseEntity.ok(reservationService.updateReservation(id, reservationDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelReservation(@PathVariable Long id) {
+        reservationService.cancelReservation(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservationDTO> getReservationById(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.getReservationById(id));
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<ReservationDTO>> getAllReservationsForUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(reservationService.getAllReservationsForUser(userId));
     }
 }
